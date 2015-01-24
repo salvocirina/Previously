186c14c3c565299bc15519e34c56bfb21817ad49
Salvo Cirinà <salvocia90@gmail.com>
2015-01-24 04:25:32 +0100

    smash hit implemented for multiple buttons

---
+++ Assets/Scripts/SmashButtonController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmashButtonController : MonoBehaviour {
+
+	public UIProgressBar smashbar;
+	public GameObject win;
+	public GameObject A;
+	public GameObject X;
+	private bool disableA;
+	private bool disableX;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("A") && !disableA) {
+			disableX = true;
+			A.SetActive (true);
+			smashbar.value += 0.15f;
+		}
+		if (Input.GetButtonDown ("X") && !disableX) {
+			disableA = true;
+			X.SetActive (true);
+			smashbar.value += 0.15f;
+		}
+		if (smashbar.value != 1) {
+			smashbar.value -= 0.01f;
+		} else {
+			win.SetActive(true);
+		}
+	}
+}
