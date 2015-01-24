0e90421bf3091fa466d7d0e2b417edb9c0f77d40
Salvo Cirinà <salvocia90@gmail.com>
2015-01-24 17:03:38 +0100

    new smash hit

--- Assets/Scripts/SmashButtonController.cs
+++ Assets/Scripts/SmashButtonController.cs
@@ -11,18 +11,24 @@ public class SmashButtonController : MonoBehaviour {
 	private bool disableX;
 	// Use this for initialization
 	void Start () {
+		A.SetActive (true);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("A") && !disableA) {
+		if (smashbar.value < 0.5f) {
 			disableX = true;
+			disableA = false;
 			A.SetActive (true);
+			X.SetActive (false);
+		if (Input.GetButtonDown ("A") && !disableA)
 			smashbar.value += 0.15f;
-		}
-		if (Input.GetButtonDown ("X") && !disableX) {
-			disableA = true;
+		}else if (smashbar.value >= 0.5f) {
+			A.SetActive (false);
 			X.SetActive (true);
+			disableA = true;
+			disableX = false;
+		if (Input.GetButtonDown ("X") && !disableX)
 			smashbar.value += 0.15f;
 		}
 		if (smashbar.value != 1) {
