cc475820e5799d49a64201429bc7a1d7c3122bde
Salvo Cirinà <salvocia90@gmail.com>
2015-01-24 17:54:05 +0100

    carrucola wip , smash hit 2 difficulty implemented

--- Assets/Scripts/SmashButtonController.cs
+++ Assets/Scripts/SmashButtonController.cs
@@ -9,6 +9,7 @@ public class SmashButtonController : MonoBehaviour {
 	public GameObject X;
 	private bool disableA;
 	private bool disableX;
+	public int difficulty;
 	// Use this for initialization
 	void Start () {
 		A.SetActive (true);
@@ -16,20 +17,31 @@ public class SmashButtonController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (smashbar.value < 0.5f) {
-			disableX = true;
-			disableA = false;
-			A.SetActive (true);
-			X.SetActive (false);
-		if (Input.GetButtonDown ("A") && !disableA)
-			smashbar.value += 0.15f;
-		}else if (smashbar.value >= 0.5f) {
-			A.SetActive (false);
-			X.SetActive (true);
-			disableA = true;
-			disableX = false;
-		if (Input.GetButtonDown ("X") && !disableX)
-			smashbar.value += 0.15f;
+		if (difficulty == 1) {
+			if (smashbar.value < 0.5f) {
+				if (Input.GetButtonDown ("A") && !disableA)
+						smashbar.value += 0.15f;
+			} else if (smashbar.value >= 0.5f) {
+				if (Input.GetButtonDown ("A") && !disableX)
+				smashbar.value += 0.10f;
+			}
+		}
+		else if(difficulty == 2){
+			if (smashbar.value < 0.5f) {
+				disableX = true;
+				disableA = false;
+				A.SetActive (true);
+				X.SetActive (false);
+			if (Input.GetButtonDown ("A") && !disableA)
+				smashbar.value += 0.15f;
+			}else if (smashbar.value >= 0.5f) {
+				A.SetActive (false);
+				X.SetActive (true);
+				disableA = true;
+				disableX = false;
+			if (Input.GetButtonDown ("X") && !disableX)
+				smashbar.value += 0.12f;
+			}
 		}
 		if (smashbar.value != 1) {
 			smashbar.value -= 0.01f;
