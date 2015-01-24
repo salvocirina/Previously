1900f8e9d92e4941c3eba720656ab8f116fe606e
Salvo Cirinà <salvocia90@gmail.com>
2015-01-24 04:56:57 +0100

    cubecontroller wip

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+	public GameObject startPosA;
+	public GameObject finalPosA;
+	public GameObject startPosX;
+	public GameObject finalPosX;
+	public GameObject cubeA;
+	public GameObject cubeX;
+//	public GameObject cubeY;
+	public GameObject activeCube;
+	// Use this for initialization
+	void Start () {
+		cubeA.transform.position = startPosA.transform.position;
+		cubeX.transform.position = startPosX.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (activeCube == cubeA) {
+			if (Input.GetAxis ("Vertical") > 0) {
+				cubeA.transform.position = finalPosA.transform.position;
+			}
+			if (Input.GetAxis ("Vertical") < 0) {
+				cubeA.transform.position = startPosA.transform.position;
+			}
+		} else if (activeCube == cubeX) {
+			if (Input.GetAxis ("Vertical") > 0) {
+				cubeX.transform.position = startPosX.transform.position;
+			}
+			if (Input.GetAxis ("Vertical") < 0) {
+				cubeX.transform.position = finalPosX.transform.position;
+			}
+		}
+		if (Input.GetButtonDown ("A")) {
+			activeCube = cubeA;
+		}
+		
+		if (Input.GetButtonDown ("X")) {
+			activeCube = cubeX;
+		}
+	}
+}
